9c9f439a8e73d0ec7c11d465d273c668a06c7d2f
Jie Yan <jieyan620@gmail.com>
2020-08-02 15:57:05 -0700

    add server

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 USE managerF_db;
 
 INSERT INTO item(itemName,isFood, isOpen, freezing, expDate, finnished) VALUES
-("apple", ture, false, false, 8/28/2020, false),
-("cherry", ture, false, false, 8/28/2020, false),
-("ice cream", ture, false, ture, 8/28/2020, ture);
+("apple", true, false, false, 8/28/2020, false),
+("cherry", true, false, false, 8/28/2020, false),
+("ice cream", true, false, true, 8/28/2020, true);
